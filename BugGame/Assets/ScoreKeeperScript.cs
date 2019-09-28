9e5f14ad6d4c3273c92b8d02ebb4d35c65a8cc86
PsyChimp <brhofmann97@yahoo.com>
2019-09-28 11:32:00 -0400

    added scorekeeper

---
+++ BugGame/Assets/ScoreKeeperScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeperScript : MonoBehaviour
+{
+    //scores are in range from 0 - 1
+    // 0 = 0%, 1 = 100%
+    List<float> scores;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scores = new List<float>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void addScore(float f)
+    {
+        scores.Add(f);
+    }
+
+    float scoreAverage()
+    {
+        float avg = 0.0f;
+        foreach (float f in scores)
+        {
+            avg += f;
+        }
+        avg /= scores.Count;
+        return avg;
+    }
+}
