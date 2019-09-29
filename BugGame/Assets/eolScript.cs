b4d045da6aee47b2bb6b6225b2df166305b7afcd
PsyChimp <brhofmann97@yahoo.com>
2019-09-29 13:23:57 -0400

    added gameover in menu

---
+++ BugGame/Assets/eolScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class eolScript : MonoBehaviour
+{
+    public GameObject gameOverScreen;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            gameOverScreen.SetActive(true);
+        }
+    }
+}
