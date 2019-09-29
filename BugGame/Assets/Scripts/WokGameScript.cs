8eb57fcce3fe84feb4ec0e7140bee407f2dfcfd0
CrystalWolfFang <adudra1@gmail.comm>
2019-09-29 09:44:00 -0400

    wok game close to done

--- BugGame/Assets/Scripts/WokGameScript.cs
+++ BugGame/Assets/Scripts/WokGameScript.cs
@@ -8,6 +8,11 @@ public class WokGameScript : MonoBehaviour
     public Sprite numTwo;
     public Sprite numOne;
     public Sprite go;
+    public GameObject RightArrowObject;
+    public GameObject LeftArrowObject;
+    public GameObject SpaceObject;
+
+    private float spawnTimer = 4.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,8 +20,10 @@ public class WokGameScript : MonoBehaviour
         StartCountdown();
     }
 
+    // This will play first so the game doesn't just start as soon as the game opens. 
+    // Giving player a chance to get ready to play.
     void StartCountdown()
-    {
+    {  
         IEnumerator ExecuteAfterTime(float time)
         {
             yield return new WaitForSeconds(time);
@@ -30,13 +37,30 @@ public class WokGameScript : MonoBehaviour
             yield return new WaitForSeconds(time);
             countdown.SetActive(false);
         }
-        StartCoroutine(ExecuteAfterTime(1));
-        
+        StartCoroutine(ExecuteAfterTime(1)); // Change number for longer pause for countdown.
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0)
+        {
+            spawnTimer = Random.Range(0.1f, 0.7f);
+            int which = Random.Range(1, 4);
+            if (which == 1)
+            {
+                Instantiate(RightArrowObject);
+            }
+            else if (which == 2)
+            {
+                Instantiate(SpaceObject);
+            }
+            else
+            if (which == 3)
+            {
+                Instantiate(LeftArrowObject);
+            }
+        }
     }
 }
