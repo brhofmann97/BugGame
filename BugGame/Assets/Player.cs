c431ebe4bfc8cda142d621a3f11a0fc38b80b89d
PsyChimp <brhofmann97@yahoo.com>
2019-09-29 12:37:20 -0400

    Added more player stuff

--- BugGame/Assets/Player.cs
+++ BugGame/Assets/Player.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     StopWatch JumpWatch;
+    StopWatch AttackWatch;
     // Start is called before the first frame update
     Rigidbody2D rb;
     BoxCollider2D bc;
-    float speed = 200f;
+    Camera cam;
+    float speed = 10f;
     List<GameObject> collisions;
     Animator ani;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         bc = GetComponent<BoxCollider2D>();
-        JumpWatch = GetComponent<StopWatch>();
         collisions = new List<GameObject>();
         ani = GetComponent<Animator>();
+        JumpWatch = GetComponent<StopWatch>();
+        AttackWatch = GetComponent<StopWatch>();
+        cam = Camera.main;
         JumpWatch.SetTimer(0.1f);
+        AttackWatch.SetTimer(0.1f);
         JumpWatch.StartClock();
+        AttackWatch.StartClock();
     }
 
     // Update is called once per frame
@@ -27,8 +33,10 @@ public class Player : MonoBehaviour
     {
         var input = Input.GetAxis("Horizontal");
         var movement = input * speed;
-        
-        if(movement != 0)
+        Vector3 camPos = cam.transform.position;
+        Vector3 playPos = this.transform.position;
+        cam.transform.position = new Vector3(playPos.x,playPos.y + 1,camPos.z);
+        if (movement != 0)
         {
             if (movement > 0)
             {
@@ -48,8 +56,22 @@ public class Player : MonoBehaviour
         if(Input.GetKey(KeyCode.W) & CanJump()){
             ani.SetBool("jump", true);
             JumpWatch.ResetClock();
-            rb.AddForce(new Vector2(0,10000));
+            rb.AddForce(new Vector2(0,500));
         }
+        if (Input.GetKey(KeyCode.Space) & CanAttack())
+        {
+            ani.SetTrigger("attack");
+        }
+    }
+
+    bool CanAttack()
+    {
+        if (AttackWatch.CheckClock())
+        {
+            AttackWatch.ResetClock();
+            return true;
+        }
+        return false;
     }
 
     bool CanJump()
@@ -58,7 +80,7 @@ public class Player : MonoBehaviour
         {
             foreach (GameObject o in collisions)
             {
-                if (o.name == "Floor")
+                if (o.name == "Floor" || o.tag == "floor")
                 {
                     ani.SetBool("jump", false);
                     return true;
@@ -66,6 +88,11 @@ public class Player : MonoBehaviour
             }
         }
         return false;
+    }
+
+    void Hurt()
+    {
+        ani.SetTrigger("hurt");
     }
 
     private void OnCollisionEnter2D(Collision2D col)
